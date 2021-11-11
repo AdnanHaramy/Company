2f3eb60b1226eb434b15b30eeb8f1d60535681c4
Adnan Haramy <82480594+AdnanHaramy@users.noreply.github.com>
2021-11-11 22:55:16 +0200

    Add files via upload

---
+++ Components/cardGrid/card-grid.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import '../cardGrid/card-grid.css'
+const CardGrid = () => (
+    <div className='cardGridContainer'>
+        <div className='cardChild'>
+            <div>
+                <p className='cardGridHeading' >Solar Products</p>
+            </div>
+            <hr className='cardHorizantalRow' />
+            <p className='cardGridParagraph' >SEIA Products</p>
+            <p className='cardGridParagraph2' >
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores earum placeat ipsum minima sapiente maxime,
+                quisquam nam ducimus nulla ad magni soluta consequatur saepe sint in exercitationem, perferendis dolorem. Assumenda!
+            </p>
+        </div>
+        <div className='cardChild'>
+            <div>
+                <p className='cardGridHeading' >Solar Products</p>
+            </div>
+            <hr className='cardHorizantalRow' />
+            <p className='cardGridParagraph' >SEIA Products</p>
+            <p className='cardGridParagraph2' >
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores earum placeat ipsum minima sapiente maxime,
+                quisquam nam ducimus nulla ad magni soluta consequatur saepe sint in exercitationem, perferendis dolorem. Assumenda!
+            </p>
+        </div>
+        <div className='cardChild'>
+            <div>
+                <p className='cardGridHeading' >Solar Products</p>
+            </div>
+            <hr className='cardHorizantalRow' />
+            <p className='cardGridParagraph' >SEIA Products</p>
+            <p className='cardGridParagraph2' >
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores earum placeat ipsum minima sapiente maxime,
+                quisquam nam ducimus nulla ad magni soluta consequatur saepe sint in exercitationem, perferendis dolorem. Assumenda!
+            </p>
+        </div>
+
+    </div>
+)
+export default CardGrid;
